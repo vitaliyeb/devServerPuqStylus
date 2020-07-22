e3b0c6c92ea9b3dd478d7a16e14ce29ecf70b1dc
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-07-22 11:19:08 +0500

    приступил к фоотеру

--- dev/styl/main.js
+++ dev/styl/main.js
@@ -1,4 +1,7 @@
-import './tariff-category.styl';
 import './common.styl';
+import './tariff-category.styl';
 import './header.styl';
-import './testDomain.styl'
+import './testDomain.styl';
+import './advantages.styl';
+import './specOff.styl';
+import './pageText.styl';
